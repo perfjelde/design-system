d8431174dc5f22a0a3a2f915366d43dd1137a97a
Per Fjelde <perfjelde@hotmail.com>
2023-12-01 18:04:15 +0100

    added storybook testing-library

--- src/Button/Button.stories.jsx
+++ src/Button/Button.stories.jsx
@@ -4,6 +4,9 @@ import { Button } from './Button';
 import { Icon } from '../Icon/Icon';
 import { StoryLinkWrapper } from '../LinkWrapper';
 
+import { userEvent, within } from '@storybook/testing-library';
+import { expect } from '@storybook/jest';
+
 
 const CustomButton = styled.button`
   border: 1px solid green;
@@ -326,4 +329,28 @@ export const AnchorWrapper = {
     </div>
   ),
 };
+
+/*
+ * New story using the play function.
+ * See https://storybook.js.org/docs/react/writing-stories/play-function
+ * to learn more about the play function.
+ */
+export const WithInteractions = {
+     args: {
+       appearance: 'primary',
+       href: 'http://storybook.js.org',
+       ButtonWrapper: StoryLinkWrapper,
+       children: 'Button',
+     },
+     play: async ({ canvasElement }) => {
+       // Assigns canvas to the component root element
+       const canvas = within(canvasElement);
+       await userEvent.click(canvas.getByRole('link'));
+       expect(canvas.getByRole('link')).toHaveAttribute(
+         'href',
+         'http://storybook.js.org',
+       );
+     },
+   };
+
 
